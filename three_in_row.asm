2dfef3102ce5386247bdeff77947d7954f1ed880
Margaret <med7068@rit.edu>
2016-03-19 14:37:29 -0400

    input validation for square values is implemented and works, construction of the board array also is implemented but cannot be tested until the print board function is written.

--- three_in_row.asm
+++ three_in_row.asm
@@ -53,18 +53,19 @@ board_arr:
 #
 
 main:
-	addi	$sp, $sp, -12 		#make space for return address
+	addi	$sp, $sp, -16 		#make space for return address
 	sw	$ra, 0($sp)	
 	sw	$s0, 4($sp)
 	sw	$s1, 8($sp)
-	
-	li	$v0, READ_INT		#get the dimension
+	sw	$s2, 12($sp)
+
+	ori	$v0, $zero, READ_INT	#get the dimension
 	syscall
 
 	or	$s0, $v0, $zero		#check value for evenness
 	andi	$t0, $s0, 1		#will be 0 if even, 1 if odd
 	beq	$t0, $zero, even
-	li	$a0, INVALID_BOARD_SIZE
+	ori	$a0, $zero, INVALID_BOARD_SIZE
 	jal	print_predef		#print out our error message
 	j	main_done		#get out of the program
 even:
@@ -73,26 +74,47 @@ even:
 	add	$t1, $t0, $t1		#if less than 2, will be 2
 					#if greater than 10 will be 0
 					#if in range will be 1
-	li	$t0, 1
-	beq	$t1, $t0, dim_valid	#break on within range
-	li 	$a0, INVALID_BOARD_SIZE	#print our error message
-	jal	print_predef
+
+	xori	$t1, $t1, 1		#if in range will be 0
+	beq	$t1, $zero, dim_valid	#break on within range
+	ori 	$a0, $zero, INVALID_BOARD_SIZE	
+	jal	print_predef		#print our error message
 	j	main_done
 dim_valid:	
-	and	$t0, $t0, $zero		#set our loop control variable to 0
-	la	$s1, board_arr		#get our memory for the board array
-board_loop:
-#	beq	$t0, $v0, board_loop_done
+	and	$s2, $t0, $zero		#set our loop control variable to 0
+	la	$a0, board_arr		#get our memory for the board array
+	mul	$s0, $s0
+	mflo	$s1			#s1 is now n^2
 	
-
-#	j	board_loop		#back to top
+board_loop:
+	beq	$s2, $s1, board_loop_done
+	ori	$v0, $zero, READ_INT	#get our next square
+	syscall	
+	or	$a1, $zero, $v0		#get ready to pass it to be checked
+	jal	enter_square_value
+	addi	$s2, $s2, 1
+	addi	$a0, $a0, 4		#integer is a byte
+	j	board_loop		#back to top
 board_loop_done:
+	ori	$a0, $zero, PROGRAM_BANNER	#print our banner
+	jal	print_predef
+	ori	$a0, $zero, INITIAL_PUZZLE	#print header
+	jal	print_predef
+	# print board
+	
+	
+	#solve
 
+	#solution over
+	
+	ori	$a0, $zero, FINAL_PUZZLE
+	#print board
 main_done:
+	lw	$s2, 12($sp)
 	lw	$s1, 8($sp)
 	lw	$s0, 4($sp)		#restore stack
 	lw	$ra, 0($sp)
-	addi	$sp, $sp, 12	
+	addi	$sp, $sp, 16	
 	jr	$ra			#return
 #
 # Name: enter_square_value
@@ -107,6 +129,28 @@ main_done:
 #
 
 enter_square_value:
+	addi	$sp, $sp, -8
+	sw	$ra, 0($sp)
+	sw	$a0, 4($sp)		#main will want this to persist
 
-
+	slt	$t0, $a1, $zero		#check if below zero
+	slti	$t1, $a1, 3		#check if below 3
+	add	$t1, $t1, $t0		#will be 1 if within range, else 0 or 2
+	xori	$t1, $t1, 1		#will be 0 if within range, else 1 or 2
 	
+	beq	$t1, $zero, valid	#break on valid
+	ori	$a0, $zero, INVALID_INPUT_VALUE
+	jal	print_predef		#give error message
+	ori	$v0, $zero, EXIT
+	syscall				#forcibly exit the program
+valid:
+	sw	$a1, 0($a0)		#a0 will have never been touched
+					#if we get this far
+	
+	lw	$a0, 4($sp)
+	lw	$ra, 0($sp)
+	addi	$sp, $sp, 8
+	jr	$ra
+
+
+
