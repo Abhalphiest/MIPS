8efc9aa3b2bdf4e9b2069c5aaa47ebda107261a3
Margaret <med7068@rit.edu>
2016-03-19 12:01:52 -0400

    dimension entry and validation implemented and tested, will work on building the board next

--- three_in_row.asm
+++ three_in_row.asm
@@ -37,8 +37,6 @@ NEWLINE	=		6
 board_arr:
 	.space 400		#the maximum size we would need for our
 				#board, 10x10x4 bytes
-dimension:
-	.word 0			#will hold the dimension of the board
 
 
 	.text
@@ -55,27 +53,47 @@ dimension:
 #
 
 main:
-	addi	$sp, $sp, -8 	#make space for return address
+	addi	$sp, $sp, -12 		#make space for return address
 	sw	$ra, 0($sp)	
 	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
 	
-#	li	$v0, READ_INT	#get the dimension
-#	syscall
+	li	$v0, READ_INT		#get the dimension
+	syscall
 
-#	or	$s0, $v0, $zero	#check value	
-
-#	and	$t0, $t0, $zero	#set our loop control variable to 0
+	or	$s0, $v0, $zero		#check value for evenness
+	andi	$t0, $s0, 1		#will be 0 if even, 1 if odd
+	beq	$t0, $zero, even
+	li	$a0, INVALID_BOARD_SIZE
+	jal	print_predef		#print out our error message
+	j	main_done		#get out of the program
+even:
+	slti	$t0, $s0, 2		#less than 2
+	slti	$t1, $s0, 11		#less than 11
+	add	$t1, $t0, $t1		#if less than 2, will be 2
+					#if greater than 10 will be 0
+					#if in range will be 1
+	li	$t0, 1
+	beq	$t1, $t0, dim_valid	#break on within range
+	li 	$a0, INVALID_BOARD_SIZE	#print our error message
+	jal	print_predef
+	j	main_done
+dim_valid:	
+	and	$t0, $t0, $zero		#set our loop control variable to 0
+	la	$s1, board_arr		#get our memory for the board array
 board_loop:
 #	beq	$t0, $v0, board_loop_done
-	li	$a0, PROGRAM_BANNER	
-	jal	print_predef
+	
+
+#	j	board_loop		#back to top
 board_loop_done:
 
-
-	lw	$s0, 4($sp)	#restore stack
+main_done:
+	lw	$s1, 8($sp)
+	lw	$s0, 4($sp)		#restore stack
 	lw	$ra, 0($sp)
-	addi	$sp, $sp, 8	
-	jr	$ra		#return
+	addi	$sp, $sp, 12	
+	jr	$ra			#return
 #
 # Name: enter_square_value
 #
@@ -91,16 +109,4 @@ board_loop_done:
 enter_square_value:
 
 
-#
-# Name: error
-#
-# Description: 	Prints an error message to the screen, cleans up memory,
-#		then exits.
-#
-# Arguments:	$a0 is the address of the error message to print
-#
-# Returns:	nothing
-#
-
-error:
 	
